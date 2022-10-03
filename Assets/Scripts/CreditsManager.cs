eb48fa533b1dbb57e014d508864650fc071b6ddd
BeeJayyy <87001491+BeeJayyy@users.noreply.github.com>
2022-10-03 18:51:31 +1100

    Menu and Other Sounds

--- Assets/Scripts/CreditsManager.cs
+++ Assets/Scripts/CreditsManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditsManager : MonoBehaviour
 {
@@ -20,6 +21,10 @@ public class CreditsManager : MonoBehaviour
         {
             transform.position += Vector3.up * scrollSpeed * Time.deltaTime;
         }
+		else
+		{
+			SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+		}
         //print(transform.localPosition.y);
     }
 }
